3a6a420450d10317b18ff332a4b38e73c3cd8a71
kitta65 <26474260+kitta65@users.noreply.github.com>
2024-02-25 10:57:48 +0900

    output full lineage graph

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,66 @@
+import * as fs from "fs/promises";
 import * as core from "@actions/core";
-import * as github from "@actions/github";
+// import * as github from "@actions/github";
 import { exec } from "child_process";
 import * as process from "process";
+
+export type Manifest = {
+  child_map: string;
+  nodes: { [key: string]: unknown };
+  sources: { [key: string]: unknown };
+  exposures: { [key: string]: unknown };
+};
 
 export async function main() {
   const dbtDirectory = core.getInput("dbt-directory");
   process.chdir(dbtDirectory);
 
+  // create manifest.json
   const dbtVersion = core.getInput("dbt-version");
+  await exec(`pipx run --spec dbt-postgres==${dbtVersion} dbt deps`);
   // TODO support other adapters
   await exec(`pipx run --spec dbt-postgres==${dbtVersion} dbt ls`);
 
-  // TODO return lineage.mermaid
+  const manifest: Manifest = await fs
+    .readFile("./manifest.json")
+    .then((buffer) => String(buffer))
+    .then((json) => JSON.parse(json));
+  const outpath = `${process.cwd()}/lineage.mermaid`;
+  const filepath = draw(outpath, manifest);
   core.setOutput("filepath", `${process.cwd()}/target/manifest.json`);
+}
+
+async function draw(outpath: string, manifest: Manifest) {
+  const resources = {
+    ...manifest.nodes,
+    ...manifest.sources,
+    ...manifest.exposures,
+  };
+  const name2id: { [key: string]: number } = {};
+  let id = 0;
+  for (const name of Object.keys(resources)) {
+    id++;
+    name2id[name] = id;
+  }
+
+  const statements: string[] = [];
+  statements.push("classDef source fill:green,stroke-width:0px,color:white");
+  statements.push("classDef model fill:blue,stroke-width:0px,color:white");
+  statements.push("classDef exposure fill:orange,stroke-width:0px,color:white");
+
+  for (const [name, id] of Object.entries(name2id)) {
+    const type = name.split(".")[0];
+    statements.push(`${id}("${name}")`);
+    statements.push(`class ${id} ${type}`);
+  }
+
+  for (const [parent, children] of Object.entries(manifest.child_map)) {
+    for (const child of children) {
+      statements.push(`${name2id[parent]} --> ${name2id[child]}`);
+    }
+  }
+
+  const mermaid =
+    "flowchart LR\n" + statements.map((stmt) => "  " + stmt + ";\n").join("");
+  await fs.writeFile(outpath, mermaid);
 }
