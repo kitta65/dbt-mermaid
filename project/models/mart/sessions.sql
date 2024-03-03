585797d23da6d03f499fd753f49ba9ca39bc9b73
dr666m1 <26474260+kitta65@users.noreply.github.com>
2024-03-03 19:22:27 +0900

    modify project

--- project/models/mart/sessions.sql
+++ project/models/mart/sessions.sql
@@ -1,8 +1,10 @@
 with
     events as (select * from {{ ref("stg_events") }}),
 
+    uses as (select * from {{ ref("stg_users") }}),
+
     sessions as (
-        select session_id, min(ts) as min_ts, max(ts) as max_ts from events group by 1
+        select session_id, min(ts) as min_ts, max(ts) as max_ts from events group by session_id
     )
 
 select *
