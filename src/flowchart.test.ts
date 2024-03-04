1d2cb5d0f6e915862b8859268dd157d687c45faf
sekine <26474260+kitta65@users.noreply.github.com>
2024-03-05 00:35:22 +0900

    ignore generic test (#34)
    * add generic test
    
    * ignore generic test
    
    * fix
    
    * fix
    
    * add todo comment
    
    * define flowchart.ts
    
    * compare with another flowchart
    
    * rm manifest.ts, use flowchart.ts
    
    * fix

--- src/flowchart.test.ts
+++ src/flowchart.test.ts
@@ -1,66 +1,124 @@
-import { Manifest } from "./manifest";
+import { Flowchart } from "./flowchart";
 import { b2a } from "./utils";
 
 describe("flowchart", () => {
   test("draw empty flowchart", () => {
-    const manifest = new Manifest({
+    const flowchart = new Flowchart({
       sources: {},
       nodes: {},
       exposures: {},
       child_map: {},
-      parent_map: {},
     });
-    const actual = manifest.flowchart(true);
+    const actual = flowchart.plot(true);
     const expected = `flowchart LR
 `;
     expect(actual).toBe(expected);
   });
 
-  test("draw simple flowchart", () => {
-    const a = "source.project.a.a";
+  test("simple vertices", () => {
+    const a = "source.project.a";
     const b = "model.project.b";
     const c = "exposure.project.c";
-    const manifest = new Manifest({
+    const flowchart = new Flowchart({
       sources: {
         [a]: {},
       },
       nodes: {
-        [b]: { checksum: { checksum: "anyvalue" } },
+        [b]: { checksum: { checksum: "b" } },
       },
       exposures: {
         [c]: {},
       },
-      child_map: {
-        [a]: [b],
-        [b]: [c],
-      },
-      parent_map: {}, // not needed when drawing entire lineage
+      child_map: {},
     });
-    const actual = manifest.flowchart(true);
+    const actual = flowchart.plot(true);
     const expected = `flowchart LR
-  ${b2a(a)}("a.a");
+  ${b2a(a)}("a");
   style ${b2a(a)} color:white,stroke:black,fill:green,stroke-width:0px;
   ${b2a(b)}("b");
   style ${b2a(b)} color:white,stroke:black,fill:blue,stroke-width:0px;
   ${b2a(c)}("c");
   style ${b2a(c)} color:white,stroke:black,fill:orange,stroke-width:0px;
-  ${b2a(a)} --> ${b2a(b)};
-  ${b2a(b)} --> ${b2a(c)};
 `;
     expect(actual).toBe(expected);
   });
 
-  test("draw entire flowchart with another manifest", () => {
-    const a = "source.project.a.a";
+  test("simple edges", () => {
+    const a = "source.project.a";
     const b = "model.project.b";
-    const c = "exposure.project.c";
-    const d = "exposure.project.d";
-    const originalManifest = new Manifest({
+    const flowchart = new Flowchart({
       sources: {
         [a]: {},
       },
       nodes: {
-        [b]: { checksum: { checksum: "original hash" } },
+        [b]: { checksum: { checksum: "b" } },
+      },
+      exposures: {},
+      child_map: {
+        [a]: [b],
+      },
+    });
+    const actual = flowchart.plot(true);
+    const expected = `flowchart LR
+  ${b2a(a)}("a");
+  style ${b2a(a)} color:white,stroke:black,fill:green,stroke-width:0px;
+  ${b2a(b)}("b");
+  style ${b2a(b)} color:white,stroke:black,fill:blue,stroke-width:0px;
+  ${b2a(a)} --> ${b2a(b)};
+`;
+    expect(actual).toBe(expected);
+  });
+
+  test("compare with identical flowchart", () => {
+    const a = "source.project.a";
+    const b = "model.project.b";
+    const original = new Flowchart({
+      sources: {
+        [a]: {},
+      },
+      nodes: {
+        [b]: { checksum: { checksum: "b" } },
+      },
+      exposures: {},
+      child_map: {
+        [a]: [b],
+      },
+    });
+    const modified = new Flowchart({
+      sources: {
+        [a]: {},
+      },
+      nodes: {
+        [b]: { checksum: { checksum: "b" } },
+      },
+      exposures: {},
+      child_map: {
+        [a]: [b],
+      },
+    });
+    modified.compare(original);
+    const actual = modified.plot(true);
+    const expected = `flowchart LR
+  ${b2a(a)}("a");
+  style ${b2a(a)} color:white,stroke:black,fill:green,stroke-width:0px;
+  ${b2a(b)}("b");
+  style ${b2a(b)} color:white,stroke:black,fill:blue,stroke-width:0px;
+  ${b2a(a)} --> ${b2a(b)};
+`;
+    expect(actual).toBe(expected);
+  });
+
+  test("compare with modified flowchart (entire)", () => {
+    const a = "source.project.a.a";
+    const b = "model.project.b";
+    const c = "exposure.project.c";
+    const d = "exposure.project.d";
+    const original = new Flowchart({
+      sources: {
+        [a]: {},
+      },
+      nodes: {
+        [b]: { checksum: { checksum: "b" } },
       },
       exposures: {
         [c]: {},
@@ -69,14 +127,13 @@ describe("flowchart", () => {
         [a]: [b],
         [b]: [c],
       },
-      parent_map: {}, // not needed when drawing entire lineage
     });
-    const modifiedManifest = new Manifest({
+    const modified = new Flowchart({
       sources: {
         [a]: {},
       },
       nodes: {
-        [b]: { checksum: { checksum: "modified hash" } },
+        [b]: { checksum: { checksum: "B" } },
       },
       exposures: {
         [d]: {},
@@ -85,9 +142,9 @@ describe("flowchart", () => {
         [a]: [b],
         [b]: [d],
       },
-      parent_map: {}, // not needed when drawing entire lineage
     });
-    const actual = modifiedManifest.flowchart(true, originalManifest);
+    modified.compare(original);
+    const actual = modified.plot(true);
     const expected = `flowchart LR
   ${b2a(a)}("a.a");
   style ${b2a(a)} color:white,stroke:black,fill:green,stroke-width:0px;
@@ -104,7 +161,8 @@ describe("flowchart", () => {
 `;
     expect(actual).toBe(expected);
   });
-  test("draw partial flowchart with another manifest", () => {
+
+  test("compare with modified flowchart (partial)", () => {
     const a1 = "source.p.a1";
     const b1 = "source.p.b1";
     const c1 = "model.p.c1";
@@ -154,12 +212,13 @@ describe("flowchart", () => {
         [e2]: [d2],
       },
     };
-    const originalManifest = new Manifest(originalData);
+    const originalChart = new Flowchart(originalData);
     const modifiedData = structuredClone(originalData);
     modifiedData.nodes[c1].checksum.checksum = "modified hash";
-    const modifiedManifest = new Manifest(modifiedData);
+    const modifiedChart = new Flowchart(modifiedData);
+    modifiedChart.compare(originalChart);
 
-    const actual = modifiedManifest.flowchart(false, originalManifest);
+    const actual = modifiedChart.plot(false);
     const expected = `flowchart LR
   ${b2a(a1)}("a1");
   style ${b2a(a1)} color:white,stroke:black,fill:green,stroke-width:0px;
@@ -179,18 +238,37 @@ describe("flowchart", () => {
     expect(actual).toBe(expected);
   });
 
-  test("do not draw unsupported resource", () => {
-    const manifest = new Manifest({
+  test("ignore unsupprted resource type", () => {
+    const flowchart = new Flowchart({
       sources: {},
-      nodes: {
-        "metrics.project.identifier": { checksum: { checksum: "anyvalue" } },
-      },
+      nodes: { "metrics.project.name": { checksum: { checksum: "abc" } } },
       exposures: {},
       child_map: {},
-      parent_map: {},
     });
-    const actual = manifest.flowchart(true);
+    const actual = flowchart.plot(true);
     const expected = `flowchart LR
+`;
+    expect(actual).toBe(expected);
+  });
+
+  test("ignore generic test", () => {
+    const model = "model.project.name";
+    const test = "test.project.name";
+    const flowchart = new Flowchart({
+      sources: {},
+      nodes: {
+        [model]: { checksum: { checksum: "anyvalue" } },
+        [test]: { checksum: { checksum: "" } },
+      },
+      exposures: {},
+      child_map: {
+        [model]: [test],
+      },
+    });
+    const actual = flowchart.plot(true);
+    const expected = `flowchart LR
+  ${b2a(model)}("name");
+  style ${b2a(model)} color:white,stroke:black,fill:blue,stroke-width:0px;
 `;
     expect(actual).toBe(expected);
   });
---
+++ src/flowchart.ts
@@ -0,0 +1,238 @@
+import * as fs from "fs/promises";
+import {
+  ManifestData,
+  SupportedResourceType,
+  Status,
+  isSupportedResourceType,
+  isNode,
+} from "./types";
+import { b2a } from "./utils";
+
+type Vertex = {
+  name: string;
+  type: SupportedResourceType;
+  status: Status;
+  hash: string;
+};
+
+type Edge = {
+  parent: string;
+  child: string;
+  status: Status;
+};
+
+export class Flowchart {
+  private vertices: Vertex[];
+  private edges: Edge[];
+  constructor(private readonly manifest: ManifestData) {
+    this.vertices = [];
+    for (const [key, value] of Object.entries({
+      ...manifest.sources,
+      ...manifest.nodes,
+      ...manifest.exposures,
+    })) {
+      const splited = key.split(".");
+      const type_ = splited[0];
+      // ignore unsupported resources
+      if (!isSupportedResourceType(type_)) continue;
+      // ignore generic test
+      if (isNode(value) && value.checksum.checksum === "") continue;
+      this.vertices.push({
+        name: key,
+        type: type_,
+        status: "identical",
+        hash: isNode(value) ? value.checksum.checksum : "",
+      });
+    }
+
+    this.edges = [];
+    for (const [parent, children] of Object.entries(manifest.child_map)) {
+      for (const child of children) {
+        if (
+          this.vertices.every((v) => v.name !== child) ||
+          this.vertices.every((v) => v.name !== parent)
+        )
+          continue;
+        this.edges.push({ parent, child, status: "identical" });
+      }
+    }
+  }
+
+  static async from(filepath: string): Promise<Flowchart> {
+    const manifest = await fs
+      .readFile(filepath)
+      .then((buffer) => String(buffer))
+      .then((json) => JSON.parse(json));
+    return new Flowchart(manifest);
+  }
+
+  compare(flowchart: Flowchart) {
+    for (const vertex of this.vertices) {
+      vertex.status = "new";
+    }
+    for (const edge of this.edges) {
+      edge.status = "new";
+    }
+
+    for (const vertex of flowchart.vertices) {
+      let isCommon = false;
+      this.vertices
+        .filter((v) => v.name === vertex.name)
+        .forEach((v) => {
+          v.status = v.hash === vertex.hash ? "identical" : "modified";
+          isCommon = true;
+        });
+      if (!isCommon) {
+        const newVertex = structuredClone(vertex);
+        newVertex.status = "deleted";
+        this.vertices.push(newVertex);
+      }
+    }
+
+    for (const edge of flowchart.edges) {
+      let isCommon = false;
+      this.edges
+        .filter((e) => e.child === edge.child && e.parent === edge.parent)
+        .forEach((e) => {
+          isCommon = true;
+          e.status = "identical";
+        });
+      if (!isCommon) {
+        const newEdge = structuredClone(edge);
+        newEdge.status = "deleted";
+        this.edges.push(newEdge);
+      }
+    }
+  }
+
+  plot(entire: boolean) {
+    const statements: string[] = [];
+    const checksheet: {
+      [key: string]: {
+        isUpstream: boolean;
+        isDownstream: boolean;
+      };
+    } = {};
+    this.vertices.forEach((vertex) => {
+      checksheet[vertex.name] = {
+        isUpstream: false,
+        isDownstream: false,
+      };
+    });
+    const markDownstream = (name: string) => {
+      if (checksheet[name].isDownstream) return;
+      checksheet[name].isDownstream = true;
+      this.edges
+        .filter((edge) => edge.parent === name)
+        .forEach((edge) => markDownstream(edge.child));
+    };
+    const markUpstream = (name: string) => {
+      if (checksheet[name].isUpstream) return;
+      checksheet[name].isUpstream = true;
+      this.edges
+        .filter((edge) => edge.child === name)
+        .forEach((edge) => markUpstream(edge.parent));
+    };
+    this.vertices.forEach((vertex) => {
+      if (vertex.status === "identical") return;
+      markDownstream(vertex.name);
+      markUpstream(vertex.name);
+    });
+
+    this.vertices.forEach((vertex) => {
+      if (
+        entire ||
+        checksheet[vertex.name].isDownstream ||
+        checksheet[vertex.name].isUpstream
+      ) {
+        statements.push(...vertexStatements(vertex));
+      }
+    });
+    this.edges.forEach((edge, idx) => {
+      if (
+        entire ||
+        checksheet[edge.parent].isDownstream ||
+        checksheet[edge.child].isUpstream
+      ) {
+        statements.push(...edgeStatements(edge, idx));
+      }
+    });
+
+    const mermaid =
+      "flowchart LR\n" + statements.map((stmt) => "  " + stmt + ";\n").join("");
+    return mermaid;
+  }
+}
+
+function vertexStatements(vertex: Vertex) {
+  const statements = [];
+  const splited = vertex.name.split(".");
+  const text = splited.slice(2).join(".");
+  const style: string[] = ["color:white", "stroke:black"];
+  switch (vertex.type) {
+    case "source":
+      style.push("fill:green");
+      break;
+    case "seed":
+      style.push("fill:blue");
+      break;
+    case "model":
+      style.push("fill:blue");
+      break;
+    case "snapshot":
+      style.push("fill:blue");
+      break;
+    case "exposure":
+      style.push("fill:orange");
+      break;
+    case "analysis":
+      style.push("fill:blue");
+      break;
+    case "test":
+      style.push("fill:blue");
+      break;
+    default:
+      throw "unnexpected resource type";
+  }
+  switch (vertex.status) {
+    case "deleted":
+      style.push("stroke-width:4px");
+      style.push("stroke-dasharray: 5 5");
+      break;
+    case "identical":
+      style.push("stroke-width:0px");
+      break;
+    case "modified":
+      style.push("stroke-width:4px");
+      break;
+    case "new":
+      style.push("stroke-width:4px");
+      break;
+    default:
+      throw "unnexpected resource status";
+  }
+  // NOTE
+  // name may contain special character (e.g. white space)
+  // which is not allowed in flowchart id
+  const id = b2a(vertex.name);
+  statements.push(`${id}("${text}")`);
+  statements.push(`style ${id} ${style.join(",")}`);
+  return statements;
+}
+
+function edgeStatements(edge: Edge, idx: number) {
+  const statements: string[] = [];
+  switch (edge.status) {
+    case "deleted":
+      statements.push(`${b2a(edge.parent)} -.-> ${b2a(edge.child)}`);
+      break;
+    case "identical":
+      statements.push(`${b2a(edge.parent)} --> ${b2a(edge.child)}`);
+      break;
+    case "new":
+      statements.push(`${b2a(edge.parent)} --> ${b2a(edge.child)}`);
+      statements.push(`linkStyle ${idx} stroke-width:4px`);
+      break;
+  }
+  return statements;
+}
